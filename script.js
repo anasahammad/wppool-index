28bba7d48fb0b9af81d18ccffa6478d860cb25e8
Anas Ahammad <anasahammad2002@gmail.com>
2024-07-06 00:51:19 +0600

    toggle menu added

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const menuButton = document.getElementById('menuButton');
-        const menuIcon = document.getElementById('menuIcon');
-        const menu = document.getElementById('menu');
 
-        menuButton.addEventListener('click', () => {
-            // Toggle the menu visibility
-            if (menu.classList.contains('hidden')) {
-                menu.classList.remove('hidden');
-                menuIcon.src = './images/bg-vector.png';
-            } else {
-                menu.classList.add('hidden');
-                menuIcon.src = 'path_to_your_menu_icon.png';
-            }
-        });
+ function sidebarOpen() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display= "block"
+}
+ function sidebarClose() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display= "none"
+}
+    
+
 
        
